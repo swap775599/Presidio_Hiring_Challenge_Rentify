05da8fbde305c381d40af10bb18d4d1aaa4357ca
swap775599 <118529628+swap775599@users.noreply.github.com>
2024-05-19 14:59:57 +0530

    Add files via upload

---
+++ backend/confi/credentials.js
@@ -0,0 +1,11 @@
+const { mongo } = require("mongoose");
+
+const mongoURI = "mongodb+srv://jayesh007va:Jayesh2003@rentify.byfgrlv.mongodb.net/rentify";
+const email = "jayesh007va@gmail.com";
+const password = "rtws hlck rszj qzdv";
+
+module.exports = {
+    mongoURI,
+    email,
+    password
+};
